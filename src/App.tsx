652292163f7903612e9062a459cda9da5399232e
Haukur Kristinsson <haukur@hauxi.is>
2021-03-22 22:19:56 +0000

    Second commit

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Verkfræðistofa</p>
+        <p>Verkfræði- og ráðgjafarfyrirtæki.</p>
       </header>
     </div>
   );
